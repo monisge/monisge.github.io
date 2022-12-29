ea52e29e0a5940b909ad4f99e7dee29bfc7d9684
monisge <2907242584@qq.com>
2022-12-29 19:30:20 +0800

    Site updated: 2022-12-29 19:30:17

--- js/custom.js
+++ js/custom.js
@@ -34,12 +34,11 @@ function percent() {
 }
 
 // 分享本页
-function share() {
-    let url = window.location.origin + window.location.pathname
-    new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
-    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
-}
-
+// function share() {
+//     let url = window.location.origin + window.location.pathname
+//     new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
+//     btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+// }
 
 
 
